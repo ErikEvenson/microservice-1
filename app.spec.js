285235e848057049f55b7e847e9faa20abe81e75
Erik Evenson <erik.e.evenson@gmail.com>
2015-07-24 17:49:04 -0500

    adds testing stub

---
+++ app.spec.js
@@ -0,0 +1,16 @@
+var
+  expect = require('chai').expect
+
+describe('users', function() {
+  before(function(done) {
+    done();
+  });
+
+  after(function(done) {
+    done();
+  });
+
+  it('should test', function() {
+    expect(true).to.be.equal(false);
+  });
+});
