4bee227450dce69eaca084783dacddcaab7a0119
Erik Evenson <erik.e.evenson@gmail.com>
2015-07-25 21:13:28 -0500

    puts packer on ci

---
+++ ci/dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+CIRCLECI_CACHE_DIR="${HOME}/bin"
+PACKER_URL="https://dl.bintray.com/mitchellh/packer/packer_0.8.2_linux_amd64.zip"
+
+if [ ! -f "${CIRCLECI_CACHE_DIR}/packer" ]; then
+  wget -O /tmp/packer.zip "${PACKER_URL}"
+  unzip -d "${CIRCLECI_CACHE_DIR}" /tmp/packer.zip
+fi
+
+packer version
